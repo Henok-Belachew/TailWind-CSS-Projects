32d7da5b4fcfcc99db77df3fb6e8bb73a51d0e30
Henok Belachew <henokbelachewstu@gmail.com>
2023-01-18 11:02:30 +0300

    JIT mode enabled

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+// Don't forget to run: "npx tailwindcss -i ./src/input.css -o ./styles.css --watch"
+// Inorder to update your style sheet to update it's self according to your change. 
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./Project-1/index.html",
+            "./Project-2/index.html",
+            "./Project-3/index.html",
+            "./Project-4/index.html",
+            "./Project-5/index.html",
+            "./Project-6/index.html",
+            "./Project-7/index.html",
+            "./Project-8/index.html",
+            "./Project-9/index.html",
+            "./index.html"],
   theme: {
     extend: {},
   },
